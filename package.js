7baac6f25ec7e1bb2a34bf86d04b5f6114b051ae
zhaoyao91 <zhaoyao91@163.com>
2017-04-26 19:20:20 +0800

    user _paramStateAsync to optimize login flow

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'zhaoyao91:accounts-wechat',
-    version: '0.0.2',
+    version: '0.1.0',
     summary: 'meteor accounts package for wechat',
     git: 'https://github.com/zhaoyao91/meteor-accounts-wechat',
     documentation: 'README.md'
